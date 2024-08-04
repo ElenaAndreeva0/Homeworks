f3f249bfa726543ebaf1d73fdf63914a5ab0ad94
ElenaAndreeva0 <makskonycev7@gmail.com>
2024-08-04 15:02:53 +0300

    Add files via upload

--- modul_6_3.py
+++ modul_6_3.py
@@ -1,100 +1,44 @@
-'''
-Цель: закрепить знания множественного наследования в Python.
-
-Задача "Мифическое наследование":
-Необходимо написать 3 класса:
-Horse - класс описывающий лошадь. Объект этого класса обладает следующими атрибутами:
-x_distance = 0 - пройденный путь.
-sound = 'Frrr' - звук, который издаёт лошадь.
-И методами:
-run(self, dx), где dx - изменение дистанции, увеличивает x_distance на dx.
-'''
-
 class Horse:
-
     def __init__(self):
         self.x_distance = 0
         self.sound = 'Frrr'
-        print('/* Запущен __init__ в Horse */')
+        super().__init__()
 
     def run(self, dx):
         self.x_distance += dx
 
-'''
-
-Eagle - класс описывающий орла. Объект этого класса обладает следующими атрибутами:
-y_distance = 0 - высота полёта
-sound = 'I train, eat, sleep, and repeat' - звук, который издаёт орёл (отсылка)
-И методами:
-fly(self, dy) где dy - изменение дистанции, увеличивает y_distance на dy.
-'''
 
 class Eagle:
-
     def __init__(self):
         self.y_distance = 0
         self.sound = 'I train, eat, sleep, and repeat'
-        print('/* Запущен __init__ в Eagle */')
+        super().__init__()
 
     def fly(self, dy):
         self.y_distance += dy
 
-'''
-Pegasus - класс описывающий пегаса. Наследуется от Horse и Eagle в том же порядке.
-Объект такого класса должен обладать атрибутами классов родителей в порядке наследования.
-Также обладает методами:
-move(self, dx, dy) - где dx и dy изменения дистанции. В этом методе должны запускаться наследованные методы run и fly соответственно.
-get_pos(self) возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance) в том же порядке.
-voice - который печатает значение унаследованного атрибута sound.
-'''
 
 class Pegasus(Horse, Eagle):
-
     def __init__(self):
         super().__init__()
-        #Horse.__init__(self) -- НЕ УВЕРЕН В ЭТОМ МЕСТЕ!
-        Eagle.__init__(self)
 
     def move(self, dx, dy):
-        self.run(dx)
-        self.fly(dy)
+        super().run(dx)
+        super().fly(dy)
 
     def get_pos(self):
-        return self.x_distance, self.y_distance
+        return (self.x_distance, self.y_distance)
 
     def voice(self):
         print(self.sound)
 
-'''
-Пункты задачи:
-Создайте классы родители: Horse и Eagle с методами из описания.
-Создайте класс наследник Pegasus с методами из описания.
-Создайте объект класса Pegasus и вызовите каждый из ранее перечисленных методов, проверив их работу.
-'''
 
-def main():
-    p1 = Pegasus()
+p1 = Pegasus()
 
-    print(p1.get_pos())
-    p1.move(10, 15)
-    print(p1.get_pos())
-    p1.move(-5, 20)
-    print(p1.get_pos())
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
 
-    p1.voice()
-
-if __name__ == '__main__':
-    main()
-'''
-Вывод на консоль:
-(0, 0)
-(10, 15)
-(5, 35)
-I train, eat, sleep, and repeat
-
-Примечания:
-Будьте внимательней, когда вызываете методы классов родителей в классе наследнике при множественном наследовании: при обращении через super() методы будут искаться сначала в первом, потом во втором и т.д. классах по mro().
-Заметьте, что Pegasus издаёт звук "I train, eat, sleep, and repeat", т.к. по порядку сначала идёт наследование от Horse, а после от Eagle.
-
-Файл module_6_3.py и загрузите его на ваш GitHub репозиторий. В решении пришлите ссылку на него.
-'''
+p1.voice()
